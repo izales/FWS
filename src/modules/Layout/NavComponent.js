9be4b632d7383e330b38e9263fe24ef80736312d
izales <i.zales@montary-agency.de>
2021-12-14 10:50:13 +0100

    css anpasungen agency

--- src/modules/Layout/NavComponent.js
+++ src/modules/Layout/NavComponent.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect,useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './css/nav_component.scss';
 import { Link } from "react-awesome-slider/dist/navigation";
 
@@ -8,11 +8,11 @@ function NavComponent(props) {
     const [slug, setSlug] = useState("/");
     let textClass = props.theme === 'dark' ? 'nav_text_dark' : 'nav_text_light';
     let navTheme = props.theme === 'dark' ? 'theme_dark' : 'theme_light';
-  
+
 
     const [hoveredSolution, setHoveredSolution] = useState(false);
     const toggleHoverSolution = () => setHoveredSolution(!hoveredSolution);
-      let visPort = hoveredSolution === true ? 'dontvisible' : '';
+    let visPort = hoveredSolution === true ? 'dontvisible' : '';
 
 
     const [hoveredAgency, setHoveredAgency] = useState(false);
@@ -32,18 +32,18 @@ function NavComponent(props) {
     const PortRef = useRef(0);
     function toggleAccordion() {
         /** */
-           
-  
-     
+
+
+
         // @ts-ignore
-         //`${contentSpace.current.scrollHeight}px`
-        
-      }
+        //`${contentSpace.current.scrollHeight}px`
+
+    }
     return (
         <div className="nav_links_div">
             <input className="menu-btn" type="checkbox" id="menu-btn" />
             <label className="menu-icon" htmlFor="menu-btn"><span className={`${navTheme} navicon`}></span></label>
-            
+
             <ul className={`${textClass} menu`}>
                 <li className="nav_logo_li"  >
                     <Link href="/">
@@ -52,51 +52,54 @@ function NavComponent(props) {
                 </li>
 
                 <li className="liPortfolio">
-                    <Link 
+                    <Link
                         href="/"
                         className={(slug === "" || slug === "/" || slug === "/portfolio-two" || slug === "/portfolio-three") ? "selected" : null}
                     >
                         Portfolio
                     </Link>
-                    <ul  ref={PortRef} className={`${visPort} dropdown`}>
+
+                    <ul ref={PortRef} className={`${visPort} dropdown`}>
+
                         <li><a href="/projects">Jagdschule</a></li>
                         <li><a href="/projects-two">Will&Apel</a></li>
                     </ul>
                 </li>
-             
-                
-                <li onMouseOver={toggleHoverSolution} onMouseLeave={toggleHoverSolution}  className="liSolution">
-                    <Link 
+
+
+                <li onMouseOver={toggleHoverSolution} onMouseLeave={toggleHoverSolution} className="liSolution">
+                    <Link
                         href="/solutions"
                         className={slug === "/solutions" ? "selected" : null}
                     >
-                        Solutions
+                        <span className='pfeil-span'>Solutions</span>
                     </Link>
-                    <ul   className={`${visSol} dropdown2`}>
+                    <ul className={`${visSol} dropdown2`}>
+                        <img className='pfeil_links' src={`${toAbsoluteUrl(`/media/icons/linker-pfeil.svg`)}`} alt="Right" />
                         <li><a href="/drohne">Drohnen</a></li>
                         <li><a href="/imagefilm">Image film</a></li>
                         <li><a href="/marketing">Marketing</a></li>
                         <li><a href="/webdesign">Webdesign</a></li>
                     </ul>
                 </li>
-               
-                <li  onMouseOver={toggleHoverAgency}  onMouseLeave={toggleHoverAgency} className="liAgency">
-                    <Link 
+
+                <li onMouseOver={toggleHoverAgency} onMouseLeave={toggleHoverAgency} className="liAgency">
+                    <Link
                         href="/agency"
                         className={slug === "/agency" ? "selected" : null}
                     >
                         Our Agency
                     </Link>
-                   
+
                 </li>
                 <li onMouseEnter={toggleHoverContact} onMouseLeave={toggleHoverContact} className="liContact">
-                    <Link 
+                    <Link
                         href="/contact-us"
                         className={slug === "/contact-us" ? "selected" : null}
                     >
                         Contact Us
                     </Link>
-                 
+
                 </li>
             </ul>
         </div>
--- src/modules/Team/Team.js
+++ src/modules/Team/Team.js
@@ -9,73 +9,73 @@ import ImprintLink from "../Layout/ImprintLink";
 function Team(props) {
 
     //First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-   
-const ref = useRef(null);
+
+    const ref = useRef(null);
 
 
 
 
-const [active, setActiveState] = useState(false);
-console.log(active)
+    const [active, setActiveState] = useState(false);
+    console.log(active)
 
-function toggleClass() {
-    const currentState = active;
-    setActiveState(!active);
-    console.log(currentState)
-};
-
-
-
-
-function openPop(){
-    const p = ref.current;
-    p.className = "show";
-    p.className = "icon_description";
-
-   
-
-
-}
-
-      
- const [slug, setSlug] = useState("/contact-us"); 
+    function toggleClass() {
+        const currentState = active;
+        setActiveState(!active);
+        console.log(currentState)
+    };
 
 
 
 
- let vh = window.innerHeight * 0.01;
- // Then we set the value in the --vh custom property to the root of the document
- document.documentElement.style.setProperty('--vh', `${vh}px`);
- // We listen to the resize event
- window.addEventListener('resize', () => {
-     // We execute the same script as before
-     let vh = window.innerHeight * 0.01;
-     document.documentElement.style.setProperty('--vh', `${vh}px`);
-   });
+    function openPop() {
+        const p = ref.current;
+        p.className = "show";
+        p.className = "icon_description";
+
+
+
+
+    }
+
+
+    const [slug, setSlug] = useState("/contact-us");
+
+
+
+
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    // We listen to the resize event
+    window.addEventListener('resize', () => {
+        // We execute the same script as before
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
     return (
         <div className="solutions_div_team">
-          
+
 
             <div className="solutions_body_div">
-             
+
 
                 <div className="icons_div">
                     <div className="icons_div_one space-right">
                         <span className="icon_span">
-                            <img onClick={toggleClass} className={`round ${active ? 'showImg': null}`} src={`${toAbsoluteUrl('/media/images/basti.jpeg')}`} alt="Sebastian" />
-                            <p   className={`icon_label ${active ? 'showLabel': null}`}>Sebastian</p>
-                            <p ref={ref} className={`icon_description ${active ? 'hidden': null}`}>
-                            <a   onClick={toggleClass} className="close"></a>
+                            <img onClick={toggleClass} className={`round ${active ? 'showImg' : null}`} src={`${toAbsoluteUrl('/media/images/basti.jpeg')}`} alt="Sebastian" />
+                            <p className="icon_label_team ">Sebastian</p>
+                            <p ref={ref} className={`icon_description ${active ? 'hidden' : null}`}>
+                                <a onClick={toggleClass} className="close_team"></a>
                                 <span className="description_title">Sebastian</span>
                                 <span className="description_text">Verantwortlich für Marketing, Kundenbetreuung und alles Organisatorische. </span>
                             </p>
                         </span>
 
                         <span className="icon_span space-right">
-                           <a onClick={openPop}></a> <img onClick={toggleClass} className={`round ${active ? 'showImg': null}`} src={`${toAbsoluteUrl('/media/images/Louis.jpeg')}`} alt="Luia" />
-                            <p   className={`icon_label `}>Louis</p>
-                            <p ref={ref} className={`icon_description ${active ? 'hidden': null}`}>
-                            <a   onClick={toggleClass} className="close"></a>
+                            <a onClick={openPop}></a> <img onClick={toggleClass} className={`round ${active ? 'showImg' : null}`} src={`${toAbsoluteUrl('/media/images/Louis.jpeg')}`} alt="Luia" />
+                            <p className="icon_label_team ">Louis</p>
+                            <p ref={ref} className={`icon_description ${active ? 'hidden' : null}`}>
+                                <a onClick={toggleClass} className="close_team"></a>
                                 <span className="description_title">Louis</span>
                                 <span className="description_text">Kameraspezialist für Outdoorprojekte. Louis bewährte sich auf zahlreichen Pirsch- und Ansitzjagden. Ob Safari in Afrika, Ansitzjagd im Deutschen Forst oder turbulente Nachsuchen: Kinoreifes Material ist garantiert!</span>
                             </p>
@@ -85,22 +85,22 @@ function openPop(){
 
                     <div className="icons_div_two space-right">
                         <span className="icon_span ">
-                        <a onClick={openPop}> <img onClick={toggleClass} className={` round ${active ? 'showImg': null}`}  src={`${toAbsoluteUrl('/media/images/wesley.jpeg')}`} alt="Wesley" /> </a>
-                            <p   className={`icon_label ${active ? 'showLabel': null}`}>Wesley</p>
-                            <p ref={ref} className={`icon_description ${active ? 'hidden': null}`}>
-                            <a   onClick={toggleClass} className="close"></a>
+                            <a onClick={openPop}> <img onClick={toggleClass} className={` round ${active ? 'showImg' : null}`} src={`${toAbsoluteUrl('/media/images/wesley.jpeg')}`} alt="Wesley" /> </a>
+                            <p className={`icon_label_team ${active ? 'showLabel' : null}`}>Wesley</p>
+                            <p ref={ref} className={`icon_description ${active ? 'hidden' : null}`}>
+                                <a onClick={toggleClass} className="close_team"></a>
                                 <span className="description_title">Wesley</span>
                                 <span className="description_text">Berufsjäger  aus England mit 30 Jahren Erfahrung. Er verbindet Waidmännisches Denken mit moderner Kameraarbeit.</span>
-                                
+
                             </p>
-                           
+
                         </span>
 
                         <span className="icon_span">
-                            <img onClick={toggleClass} className={` round ${active ? 'showImg': null}`}  src={`${toAbsoluteUrl('/media/images/mathis.jpeg')}`} alt="Mathis" />
-                            <p   className={`icon_label ${active ? 'showLabel': null}`}>Mathis</p>
-                            <p ref={ref} className={`icon_description ${active ? 'hidden': null}`}>
-                            <a   onClick={toggleClass} className="close"></a>
+                            <img onClick={toggleClass} className={` round ${active ? 'showImg' : null}`} src={`${toAbsoluteUrl('/media/images/mathis.jpeg')}`} alt="Mathis" />
+                            <p className={`icon_label_team ${active ? 'showLabel' : null}`}>Mathis</p>
+                            <p ref={ref} className={`icon_description ${active ? 'hidden' : null}`}>
+                                <a onClick={toggleClass} className="close_team"></a>
                                 <span className="description_title">Mathis</span>
                                 <span className="description_text ">Mathis hat mit seinen mehr als 10 Jahren Berufserfahrung ein Auge für Details wie kein anderer. In der Post-Produktion vermag er es jeden Pixel farblich anzupassen um so ein fesselndes Gesamtkunstwerk zu erschaffen.</span>
                             </p>
@@ -114,10 +114,10 @@ function openPop(){
                         </span> */}
                     </div>
                 </div>
-          
+
             </div>
 
-           
+
         </div>
     );
 }
